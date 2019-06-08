10526599b0000ab28d44469e6c0b9785a8bcd249
destroyermakers <destroyer.makers.projects@gmail.com>
2019-06-08 23:04:46 +0000

    cambiamos direccion del lcd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var lcdi2c = require('lcdi2c');
-var lcd = new lcdi2c(1, 0x27, 16, 2);
+var lcd = new lcdi2c(1, 0x3f, 16, 2);
 var currentTime = new Date();
 lcd.clear();
 lcd.println(currentTime.toTimeString().substring(0, 8), 1);
