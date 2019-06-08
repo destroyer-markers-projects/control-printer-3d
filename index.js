bb6c47155bd31cbce2c333ea00e7370534d3b425
destroyermakers <destroyer.makers.projects@gmail.com>
2019-06-08 22:50:02 +0000

    añadimos codigo lcd

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
-
+var lcdi2c = require('lcdi2c');
+var lcd = new lcdi2c(1, 0x27, 16, 2);
+var currentTime = new Date();
+lcd.clear();
+lcd.println(currentTime.toTimeString().substring(0, 8), 1);
+lcd.println(message, 2);
