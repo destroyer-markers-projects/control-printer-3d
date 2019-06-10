40c680f7a24fb3101c591f3931f224ccb43117a2
DAVID CHIVATO FUENTE <david.chivato.fuente@es.ibm.com>
2019-06-10 19:37:06 +0200

    cambios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var lcdi2c = require('lcdi2c');
 var dht    = require('dht-sensor');
 var gpio   = require('onoff').Gpio;
 
-// Funciones dsdfasdfa
+// Funciones
 
 function PrintLCD(HUMEDAD, TEMPERATURA) {
     var currentTime = new Date();
