9bf034ad19977e553795f384564a4d8e72c01712
destroyer <51417620+destroyermakers@users.noreply.github.com>
2019-06-10 19:29:17 +0200

    Update index.js
    cambios

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-
+//Modulos requeridos
 var lcdi2c = require('lcdi2c');
-var lcd = new lcdi2c(1, 0x3f, 16, 2);
-var currentTime = new Date();
-lcd.clear();
-lcd.println(currentTime.toTimeString().substring(0, 8), 1);
-lcd.println("Hola como estas", 2);
-lcd.println("Hola como estas", 2);
+var dht    = require('dht-sensor');
+var gpio   = require('onoff').Gpio;
+
+// Funciones
+
+function PrintLCD(HUMEDAD, TEMPERATURA) {
+    var currentTime = new Date();
+    lcd.clear();
+    lcd.println(currentTime.toTimeString().substring(0, 8), 1);
+    lcd.println("T: " + TEMPERATURA + "º / H: " + HUMEDAD + "%", 2);
+    lcd.println("Hola como estas", 2);
+}
+
+//Conexiones Dispositivos
+MODE_DHT = 11     // Modelo DHT (11 o 22)
+GPIO_DHT = 23     // DHT 11
+GPIO_PIR = 4
+I2C_LCD  = "0x3f" // Dirección I2C LCD
+
+// Declaramos las variables de los dispositivos
+var lcd     = new lcdi2c(1, I2C_LCD, 20, 4);
+var dht11   = dht.read(MODE_DHT, GPIO_DHT);
+var pir     = new gpio(GPIO_PIR, 'in', 'both');
+
+//Dispisitivo DHT11
+var HUMEDAD     = dht11.humidity);
+var TEMPERATURA = dht11.temperature);
+
+
+//Dispositivo PIR (Si tiene movimiento)
+pir.watch(function(err, value) {
+    if (value == 1) {
+        PrintLCD('Intruder alert');
+    } else {
+        PrintLCD('Intruder gone');
+    }
+});
+
