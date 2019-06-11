f3c751d55c53320b8abbe8c9af232e52504c98f5
DAVID CHIVATO FUENTE <david.chivato.fuente@es.ibm.com>
2019-06-11 16:34:17 +0200

    añadimos lcd y bucle para pir

--- index.js
+++ index.js
@@ -7,33 +7,46 @@ var rp     = require('request-promise');
 
 
 
-var api = {
-    method: 'GET',
-    uri: "https://balkiest-ruff-7920.dataplicity.io/api/job?apikey=2D12C1ECED1E4314A28CD39D0AA6FAAA",
-    json: true
-};
 
-rp(api)
-    .then(function (parsedBody) {
-        //console.log(parsedBody)
-        //console.log(parsedBody.state)
-        if (parsedBody.state == "Printing"){
-            //Si la impresora esta funcionando encendemos el dispplay
-            var FICHERO = parsedBody.job.file.name
-            var PROGRESO = parsedBody.progress.completion
-            PROGRESO = PROGRESO.toFixed(2);
-            var RESTANTE = (parsedBody.progress.printTimeLeft / 60) / 60
-            RESTANTE = RESTANTE.toFixed(2);
-            console.log("Archivo: " + FICHERO + " %")
-            console.log("Progreso: " + PROGRESO + "%")
-            console.log("Restante: " + RESTANTE + " horas")
-        }
 
-    })
-    .catch(function (err) {
-        console.log(err)
-        return res.status(500).send({
-            message: "error al recuperar datos del job"
+function Movimiento() {
+
+    //Declaramos variables
+    var FICHERO  = ""
+    var PROGRESO = ""
+    var RESTANTE = ""
+    
+    var api = {
+        method: 'GET',
+        uri: "https://balkiest-ruff-7920.dataplicity.io/api/job?apikey=2D12C1ECED1E4314A28CD39D0AA6FAAA",
+        json: true
+    };
+
+    rp(api)
+        .then(function (parsedBody) {
+            if (parsedBody.state == "Printing"){
+                //Si la impresora esta funcionando encendemos el dispplay
+                FICHERO = parsedBody.job.file.name
+                PROGRESO = parsedBody.progress.completion
+                PROGRESO = PROGRESO.toFixed(2);
+                RESTANTE = (parsedBody.progress.printTimeLeft / 60) / 60
+                RESTANTE = RESTANTE.toFixed(2);
+                FICHERO  = "Archivo: " + FICHERO
+                PROGRESO = "Progreso: " + PROGRESO + " %"
+                RESTANTE = "Restante: " + RESTANTE + " horas"
+                console.log(FICHERO)
+            }
+
+        })
+        .catch(function (err) {
+            console.log(err)
+            FICHERO  = "Archivo : ERROR"
+            PROGRESO = "Progreso: ERROR"
+            RESTANTE = "Restante: ERROR"
         });
+    
+    console.log(FICHERO)
+}
 
-    });
+setInterval(Movimiento, 1500);
+
