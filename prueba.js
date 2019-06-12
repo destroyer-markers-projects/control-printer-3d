2f2dbee9ca9750f8ca19eb32fd7747178225c420
destroyer-makers <destroyer.makers.projects@gmail.com>
2019-06-12 20:29:12 +0200

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -16,10 +16,23 @@
 //	}
 //});
 
-var sensor = require("node-dht-sensor");
+//var sensor = require("node-dht-sensor");
+//
+//sensor.read(11, 4, function(err, temperature, humidity) {
+//	if (!err) {
+//		console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+//	}
+//});
 
-sensor.read(11, 4, function(err, temperature, humidity) {
-	if (!err) {
-		console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
-	}
-});
+var rpiDhtSensor = require('rpi-dht-sensor');
+ 
+var dht = new rpiDhtSensor.DHT11(2);
+ 
+function read () {
+	var readout = dht.read();
+ 
+		console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+				'humidity: ' + readout.humidity.toFixed(2) + '%');
+		setTimeout(read, 5000);
+}
+read();
