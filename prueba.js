ab3d424755dd4364d072f3919e008b04fcd66cd7
destroyer-makers <destroyer.makers.projects@gmail.com>
2019-06-12 20:22:40 +0200

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,18 +1,25 @@
-var lcdi2c = require('lcdi2c');
-var sensor = require('node-dht-sensor');
-var gpio   = require('onoff').Gpio;
+//var lcdi2c = require('lcdi2c');
+//var sensor = require('node-dht-sensor');
+//var gpio   = require('onoff').Gpio;
+//
+//var lcd = new lcdi2c(1, 0x3f, 16, 2);
+//
+//lcd.clear();
+//
+//sensor.read(22, 23, function(err, temperature, humidity) {
+//	if (!err) {
+//		console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+//			'humidity: ' + humidity.toFixed(1) + '%'
+//		);
+//		TEMPERATURA = temperature.toFixed(1) + '°C'
+//		lcd.println(TEMPERATURA, 2);
+//	}
+//});
 
-var lcd = new lcdi2c(1, 0x3f, 16, 2);
+var sensor = require("node-dht-sensor");
 
-lcd.clear();
-
-sensor.read(22, 23, function(err, temperature, humidity) {
+sensor.read(11, 4, function(err, temperature, humidity) {
 	if (!err) {
-		console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-			'humidity: ' + humidity.toFixed(1) + '%'
-		);
-		TEMPERATURA = temperature.toFixed(1) + '°C'
-		lcd.println(TEMPERATURA, 2);
+		console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
 	}
 });
-
